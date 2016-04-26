4a6a6340d8a2005fdce28d28af3e99e021526f4d
nayeonkimmie <kimnaye@gmail.com>
2016-04-26 10:40:34 -0400

    update

---
+++ jquery_exercise/main.js
@@ -0,0 +1,34 @@
+/*Create a Checklist: Independent Practice
+
+You'll add the ability to complete tasks in the list. Your program must complete the following rules:
+
+- Through jQuery add a "complete task" link at the end of each to-do item.
+- When clicked, the link will cross out the current task (hint: add a class to the list that sets the text-decoration to strikeout).
+- Each new task added by the user needs to also have the "complete task" link akt the end.
+- Bonus: when user completes task, remove the task from the current list and add it to a completed list below. Do not show this list unless the there are completed items.
+- Bonus 2: add the ability to restore items from the completed list to the first list.
+
+*/
+
+/*
+$(function() {
+});
+
+var MyApp = {};
+
+MyApp.addToList = function(list) {
+  var $newLi = $('<li>');
+  var $newItemText = $('#new-thing');
+  $newLi.html($newItemText.val());
+  $newItemText.val('');
+  if ($newLi.html() !== '') {
+    list.append($newLi);
+  }
+}
+
+*/
+
+// 1. create a form and a button that will add onto the existing list
+// 2. add a checkbox next to each list items
+// 3. make the checkbox checkable with crossout 
+
